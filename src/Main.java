44f5dd7fde81d061fa3f8375cd164272e259cb63
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-31 11:32:50 +0300

    add label to the frame

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,15 @@ public class Main {
     public static void main(String[] args) {
 
         //JLabel = a GUI display area for a string of text, an image, or both
-        JLabel label = new JLabel();
+        JLabel label = new JLabel(); //create a label
+        label.setText("Bro, do you even code?"); //set text of label
 
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
         frame.setVisible(true);
+        frame.add(label);
 
 
     }
